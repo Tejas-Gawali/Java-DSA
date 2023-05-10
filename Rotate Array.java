270dd7136d409e27a2a471998ba34d10b9ee1dd9
Tejas Gawali <106678713+Tejas-Gawali@users.noreply.github.com>
2023-05-10 11:11:32 +0530

    Create Rotate Array.java
    Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer.

---
+++ Rotate Array.java
@@ -0,0 +1,30 @@
+
+class Solution
+{
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int arr[], int d, int n)
+    {
+        
+        if (d == 0)
+            return;
+            
+            
+        d = d % n;
+        reverseArray(arr,0 , d - 1 );
+        reverseArray(arr,d , n -1 );
+        reverseArray(arr, 0 , n -1 );
+        
+        
+    }
+    
+    static void reverseArray(int arr[] , int start , int end){
+        while(start< end){
+            int temp ;
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
