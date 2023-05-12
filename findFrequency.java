7ccb130add6c804b166b3e3ebbf3a6227ef281b0
Tejas Gawali <106678713+Tejas-Gawali@users.noreply.github.com>
2023-05-12 22:40:53 +0530

    Create findFrequency.java
    Given a vector of N positive integers and an integer X. The task is to find the frequency of X in vector.

---
+++ findFrequency.java
@@ -0,0 +1,14 @@
+class Solution{
+    int findFrequency(int A[], int x){
+        
+        int n = A.length;
+        int freq = 0;
+        for(int i = 0; i < n ; i++){
+            if(A[i] == x){
+                freq++;
+            }
+        }
+        return freq;
+        
+    }
+}
