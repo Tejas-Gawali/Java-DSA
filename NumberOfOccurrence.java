ecb38c7a732777229312f5624ae6512a028ee6bf
Tejas Gawali <106678713+Tejas-Gawali@users.noreply.github.com>
2023-05-12 21:53:11 +0530

    Create NumberOfOccurrence.java
    Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.

---
+++ NumberOfOccurrence.java
@@ -0,0 +1,11 @@
+class Solution {
+    int count(int[] arr, int n, int x) {
+        int occ = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == x){
+                occ++;
+            }
+        }
+        return occ;
+    }
+}
