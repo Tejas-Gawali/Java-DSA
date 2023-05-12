95b92383de681342bf5eff8987d042f7dc525b14
Tejas Gawali <106678713+Tejas-Gawali@users.noreply.github.com>
2023-05-12 21:46:45 +0530

    Create PeakElement.java
    Given an array arr[] of size N, Return the index of any one of its peak elements.
    Note: The generated output will always be 1 if the index that you return is correct. Otherwise output will be 0.

---
+++ PeakElement.java
@@ -0,0 +1,26 @@
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+        int ans = 0;
+        if (n == 1)
+            return 0;
+        if (arr[0] >= arr[1])
+            return 0;
+        if (arr[n - 1] >= arr[n - 2])
+            return n - 1;
+            
+       for(int i = 1 ; i < n-1 ; i++){
+           if(arr[i] >= arr[i+1] && arr[i] >= arr[i-1]){
+               ans = i;
+               break;
+           }
+           
+       }
+       
+       return ans;
+    }
+}
