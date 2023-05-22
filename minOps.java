66290fc80d607da4495ae145c0c06d03b20b89bd
Tejas Gawali <106678713+Tejas-Gawali@users.noreply.github.com>
2023-05-22 23:46:58 +0530

    Create minOps.java

---
+++ minOps.java
@@ -0,0 +1,43 @@
+// Program to make all array equal
+import java.io.*;
+import java.util.Arrays;
+
+class GFG {
+	// function for calculating min operations
+	static int minOps(int arr[], int n, int k)
+	{
+		// max elements of array
+		int max = Integer.MIN_VALUE;
+		for(int i=0; i<n; i++) {
+			max = Math.max(max, arr[i]);
+		}
+		int res = 0;
+
+		// iterate for all elements
+		for (int i = 0; i < n; i++) {
+
+			// check if element can make equal to
+			// max or not if not then return -1
+			if ((max - arr[i]) % k != 0)
+				return -1;
+
+			// else update res for required operations
+			else
+				res += (max - arr[i]) / k;
+		}
+
+		// return result
+		return res;
+	}
+
+	// Driver program
+	public static void main(String[] args)
+	{
+		int arr[] = { 21, 33, 9, 45, 63 };
+		int n = arr.length;
+		int k = 6;
+		System.out.println(minOps(arr, n, k));
+	}
+}
+
+// This code is contributed by vt_m
